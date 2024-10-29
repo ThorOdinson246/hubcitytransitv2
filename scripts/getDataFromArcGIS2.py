b9210f95d7469334e389b818a8b6a37469a0319d
ThorOdinson246 <110178596+ThorOdinson246@users.noreply.github.com>
2024-10-29 18:23:01 -0500

    ..

--- scripts/getDataFromArcGIS2.py
+++ scripts/getDataFromArcGIS2.py
@@ -1,25 +1,25 @@
-import json
-from pyproj import Transformer
+# import json
+# from pyproj import Transformer
 
-def convert_coordinates(input_file, output_file):
-    # Initialize the transformer for Web Mercator to WGS84
-    transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True)
+# def convert_coordinates(input_file, output_file):
+#     # Initialize the transformer for Web Mercator to WGS84
+#     transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True)
     
-    with open(input_file, 'r') as f:
-        data = json.load(f)
+#     with open(input_file, 'r') as f:
+#         data = json.load(f)
     
-    for feature in data:
-        if 'x' in feature and 'y' in feature:
-            x, y = feature['x'], feature['y']
-            lon, lat = transformer.transform(x, y)
-            feature['x'], feature['y'] = lat, lon
+#     for feature in data:
+#         if 'x' in feature and 'y' in feature:
+#             x, y = feature['x'], feature['y']
+#             lon, lat = transformer.transform(x, y)
+#             feature['x'], feature['y'] = lat, lon
     
-    with open(output_file, 'w') as f:
-        json.dump(data, f, indent=2)
+#     with open(output_file, 'w') as f:
+#         json.dump(data, f, indent=2)
 
-    print(f"Converted coordinates saved to {output_file}")
+#     print(f"Converted coordinates saved to {output_file}")
 
-if __name__ == "__main__":
-    input_file = 'extracted_stops_copy.json'
-    output_file = 'converted_stops.json'
-    convert_coordinates(input_file, output_file)
+# if __name__ == "__main__":
+#     input_file = 'extracted_stops_copy.json'
+#     output_file = 'converted_stops.json'
+#     convert_coordinates(input_file, output_file)
--- scripts/plotMapConvertCoordinate.py
+++ scripts/plotMapConvertCoordinate.py
@@ -1,86 +1,86 @@
-import folium
-from folium.plugins import Realtime
-from pyproj import Transformer
-from utils.getDataFromArcGIS import getDataFromArcGIS
-from utils.getCurrentLocation import DeviceLocationFetcher
-from utils.deviceIDs import device_id
-from misc.routes import paths_blueroute, paths_goldrouteusm, paths_greenroute
-# Initialize the transformer to convert from EPSG:3857 to EPSG:4326
-transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326")
+# import folium
+# from folium.plugins import Realtime
+# from pyproj import Transformer
+# from utils.getDataFromArcGIS import getDataFromArcGIS
+# from utils.getCurrentLocation import DeviceLocationFetcher
+# from utils.deviceIDs import device_id
+# from misc.routes import paths_blueroute, paths_goldrouteusm, paths_greenroute
+# # Initialize the transformer to convert from EPSG:3857 to EPSG:4326
+# transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326")
 
-# instance of getDataFromArcGIS
-# Convert the coordinates from EPSG:3857 to EPSG:4326
-def convert_coordinates(paths):
-    converted_paths = [
-        [transformer.transform(x, y)[0], transformer.transform(x, y)[1]] for x, y in paths
-    ]
-    return converted_paths
+# # instance of getDataFromArcGIS
+# # Convert the coordinates from EPSG:3857 to EPSG:4326
+# def convert_coordinates(paths):
+#     converted_paths = [
+#         [transformer.transform(x, y)[0], transformer.transform(x, y)[1]] for x, y in paths
+#     ]
+#     return converted_paths
 
-# def convert_coordinates(coordinate):
+# # def convert_coordinates(coordinate):
 
-# COMMENT C@ HERE
-# get the current location of the device
-feature_layer_url = "https://utility.arcgis.com/usrsvcs/servers/b02066689d504f5f9428029f7268e060/rest/services/Hosted/8bd5047cc5bf4195887cc5237cf0d3e0_Track_View/FeatureServer/1"
-fetcher = DeviceLocationFetcher(feature_layer_url)
-location = fetcher.get_bus_location(device_id["gold1"])
-print(location)
-# UNCOMMENT C@ HERE
-
-
-# Hattiesburg coordinates
-# Map 
-m = folium.Map(location=[31.3271, -89.2903], zoom_start=13,width="1200px" , height="1200px")
+# # COMMENT C@ HERE
+# # get the current location of the device
+# feature_layer_url = "https://utility.arcgis.com/usrsvcs/servers/b02066689d504f5f9428029f7268e060/rest/services/Hosted/8bd5047cc5bf4195887cc5237cf0d3e0_Track_View/FeatureServer/1"
+# fetcher = DeviceLocationFetcher(feature_layer_url)
+# location = fetcher.get_bus_location(device_id["gold1"])
+# print(location)
+# # UNCOMMENT C@ HERE
 
 
-folium.PolyLine(locations=convert_coordinates(paths_blueroute), color='#3a72ff',).add_to(m)
-folium.PolyLine(locations=convert_coordinates(paths_goldrouteusm), color='#ffcc00').add_to(m)
-folium.PolyLine(locations=convert_coordinates(paths_greenroute), color='#53a600').add_to(m)
-folium.Marker(location=[31.3271, -89.2903], popup='Hattiesburg').add_to(m)
-folium.Marker(location=location, popup='Bluej').add_to(m)
-
-# Buttons for the routes
-folium.LayerControl().add_to(m)
+# # Hattiesburg coordinates
+# # Map 
+# m = folium.Map(location=[31.3271, -89.2903], zoom_start=13,width="1200px" , height="1200px")
 
 
-# REALTIME EXTENSION IMPLEMENTATION
-# source = folium.JsCode("""
-#     function(responseHandler, errorHandler) {
-#         var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+# folium.PolyLine(locations=convert_coordinates(paths_blueroute), color='#3a72ff',).add_to(m)
+# folium.PolyLine(locations=convert_coordinates(paths_goldrouteusm), color='#ffcc00').add_to(m)
+# folium.PolyLine(locations=convert_coordinates(paths_greenroute), color='#53a600').add_to(m)
+# folium.Marker(location=[31.3271, -89.2903], popup='Hattiesburg').add_to(m)
+# folium.Marker(location=location, popup='Bluej').add_to(m)
 
-#         fetch(url)
-#         .then((response) => {
-#             return response.json().then((data) => {
-#                 var { id, longitude, latitude } = data;
-
-#                 return {
-#                     'type': 'FeatureCollection',
-#                     'features': [{
-#                         'type': 'Feature',
-#                         'geometry': {
-#                             'type': 'Point',
-#                             'coordinates': [longitude, latitude]
-#                         },
-#                         'properties': {
-#                             'id': id
-#                         }
-#                     }]
-#                 };
-#             })
-#         })
-#         .then(responseHandler)
-#         .catch(errorHandler);
-#     }
-# """)
+# # Buttons for the routes
+# folium.LayerControl().add_to(m)
 
 
+# # REALTIME EXTENSION IMPLEMENTATION
+# # source = folium.JsCode("""
+# #     function(responseHandler, errorHandler) {
+# #         var url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-# rt = Realtime(source, interval=1000)
-# rt.add_to(m)
-# REALTIME EXTENSION IMPLEMENTATION 
+# #         fetch(url)
+# #         .then((response) => {
+# #             return response.json().then((data) => {
+# #                 var { id, longitude, latitude } = data;
+
+# #                 return {
+# #                     'type': 'FeatureCollection',
+# #                     'features': [{
+# #                         'type': 'Feature',
+# #                         'geometry': {
+# #                             'type': 'Point',
+# #                             'coordinates': [longitude, latitude]
+# #                         },
+# #                         'properties': {
+# #                             'id': id
+# #                         }
+# #                     }]
+# #                 };
+# #             })
+# #         })
+# #         .then(responseHandler)
+# #         .catch(errorHandler);
+# #     }
+# # """)
 
 
 
-# folium.Marker(location=, popup='Blue2').add_to(m)
-# Save the map to an HTML file
-m.save('index.html')
+# # rt = Realtime(source, interval=1000)
+# # rt.add_to(m)
+# # REALTIME EXTENSION IMPLEMENTATION 
+
+
+
+# # folium.Marker(location=, popup='Blue2').add_to(m)
+# # Save the map to an HTML file
+# m.save('index.html')
 
